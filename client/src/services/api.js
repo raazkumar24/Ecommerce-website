db6495e6583f3c8a8bf835f375c07a828439d73b
raazkumar24 <raazkumar625@gmail.com>
2026-01-10 21:20:17 +0530

    update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecommerce-website-2hug.onrender.com",
+  baseURL: "https://ecommerce-website-2hug.onrender.com/api",
 });
 
 // Attach token automatically
